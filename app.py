7b55f22d32f1ae5d5e780d78425e445432bef797
sopier <sopier@gmail.com>
2014-01-04 20:40:00 +0700

    edit and add

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 
+import urllib2
+
 def test():
-    return None
+    url = "https://www.google.com/search?q=toyota+owners+manual+filetype:pdf&oq=search+google+100+results&num=100"
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    html = opener.open(url).read()
+    return html
+
+print test()
