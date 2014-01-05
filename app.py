b7aad835cfa90a19e8c5b681b9fb3d74ed282f00
sopier <sopier@gmail.com>
2014-01-05 08:12:46 +0700

    adding gevent

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
+import gevent
+from gevent import monkey
+monkey.patch_all()
 import urllib2
 
-def test():
-    url = "https://www.google.com/search?q=toyota+owners+manual+filetype:pdf&oq=search+google+100+results&num=100"
+keywords = [
+        'toyota owners manual',
+        'honda owners manual',
+        'mitsubishi owners manual',
+        'suzuki owners manual',
+        'nikon owners manual',
+        'canon owners manual',
+        'python ebook',
+        'php ebook',
+        'ruby on rails tutorial',
+        'java ebook tutorial'
+        ]
+
+urls = ["https://www.google.com/search?q=" + keyword.replace(' ', '+') + "+filetype:pdf&oq=search+google+100+results&num=100" for keyword in keywords]
+
+def grab(url):
+    print 'Starting %s' % url
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     html = opener.open(url).read()
-    return html
+    print '%s: %s bytes: %r' % (url, len(html), html[:50])
 
-print test()
+jobs = [gevent.spawn(grab, url) for url in urls]
+gevent.joinall(jobs)
