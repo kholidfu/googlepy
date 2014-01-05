12ef85c27ba496228309553769f99298884ddcd9
sopier <sopier@gmail.com>
2014-01-05 17:18:23 +0700

    sdasd

--- app.py
+++ app.py
@@ -7,42 +7,56 @@ monkey.patch_all()
 import urllib2
 from bs4 import BeautifulSoup
 import re
+import pymongo
 
 keywords = [
 'toyota owners manual',
 'honda owners manual',
-'mitsubishi owners manual',
-'suzuki owners manual',
-'nikon owners manual',
-'canon owners manual',
-'python ebook',
-'php ebook',
-'ruby on rails tutorial',
-'java ebook tutorial'
-        ]
+# 'mitsubishi owners manual',
+# 'suzuki owners manual',
+# 'nikon owners manual',
+# 'canon owners manual',
+# 'python ebook',
+# 'php ebook',
+# 'ruby on rails tutorial',
+# 'java ebook tutorial'
+]
 
-urls = ["https://www.google.com/search?q=" + keyword.replace(' ', '+') + "+filetype:pdf&oq=search+google+100+results&num=100" for keyword in keywords]
+google_urls = ["https://www.google.com/search?q=" + keyword.replace(' ', '+') + "+filetype:pdf&oq=search+google+100+results&num=100" for keyword in keywords]
 
 def grab(url):
     print 'Starting %s' % url
+    # http request
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     html = opener.open(url).read()
-    #print '%s: %s bytes: %r' % (url, len(html), html[:50])
+
     # soup coming in
     soup = BeautifulSoup(html)
+
     # parsing
+    ## keywords
+    keys = keywords
+
+    ## get title
     titles = [i.get_text() for i in soup.findAll('h3', attrs={'class': 'r'})]
+
+    ## get url
     ahrefs = [i.find('a')['href'] for i in soup.findAll('h3', attrs={'class': 'r'})]
     pattern = re.compile(r"=(.*?)&")
-    #re.search(re.compile(r"q=(.*?)&"), u).group(1)
     urls = [re.search(pattern, i).group(1) for i in ahrefs]
-    snippets = [i.get_text() for i in soup.findAll('span', attrs={'class': 'st'})]
-    print titles[0]
-    print urls[0]
-    print snippets[0]
-    print '=' * 50
-    print
 
-jobs = [gevent.spawn(grab, url) for url in urls]
+    ## get snippet
+    snippets = [i.get_text() for i in soup.findAll('span', attrs={'class': 'st'})]
+    print titles[:3]
+    print urls[:3]
+
+
+
+
+jobs = [gevent.spawn(grab, url) for url in google_urls]
 gevent.joinall(jobs)
+
+# make sure the length between title, url, and snippets are the same
+# and not null
+# insert into mongodb
