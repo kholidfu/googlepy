c58627dcaf4a1f49602ddda81573e31f844aaa98
sopier <sopier@gmail.com>
2014-01-05 10:17:06 +0700

    begin parsing

--- app.py
+++ app.py
@@ -3,19 +3,22 @@
 import gevent
 from gevent import monkey
 monkey.patch_all()
+
 import urllib2
+from bs4 import BeautifulSoup
+import re
 
 keywords = [
-        'toyota owners manual',
-        'honda owners manual',
-        'mitsubishi owners manual',
-        'suzuki owners manual',
-        'nikon owners manual',
-        'canon owners manual',
-        'python ebook',
-        'php ebook',
-        'ruby on rails tutorial',
-        'java ebook tutorial'
+'toyota owners manual',
+'honda owners manual',
+'mitsubishi owners manual',
+'suzuki owners manual',
+'nikon owners manual',
+'canon owners manual',
+'python ebook',
+'php ebook',
+'ruby on rails tutorial',
+'java ebook tutorial'
         ]
 
 urls = ["https://www.google.com/search?q=" + keyword.replace(' ', '+') + "+filetype:pdf&oq=search+google+100+results&num=100" for keyword in keywords]
@@ -25,7 +28,16 @@ def grab(url):
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     html = opener.open(url).read()
-    print '%s: %s bytes: %r' % (url, len(html), html[:50])
+    #print '%s: %s bytes: %r' % (url, len(html), html[:50])
+    # soup coming in
+    soup = BeautifulSoup(html)
+    # parsing
+    titles = [i.get_text() for i in soup.findAll('h3', attrs={'class': 'r'})]
+    ahrefs = [i.find('a')['href'] for i in soup.findAll('h3', attrs={'class': 'r'})]
+    pattern = re.compile(r"=(.*?)&")
+    #re.search(re.compile(r"q=(.*?)&"), u).group(1)
+    urls = [re.search(pattern, i).group(1) for i in ahrefs]
+    print urls[0]
 
 jobs = [gevent.spawn(grab, url) for url in urls]
 gevent.joinall(jobs)
