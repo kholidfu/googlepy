881e8d65c3fe8c28b1258276cdf55d9ad6a99b21
sopier <sopier@gmail.com>
2014-01-07 13:45:43 +0700

    insert term into mongodb

--- terms_harvester.py
+++ terms_harvester.py
@@ -1,16 +1,31 @@
 import urllib2
 from bs4 import BeautifulSoup
+import pymongo
+import time
+
+c = pymongo.Connection()
+db = c['terms']
 
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 # ebookbrowse
-html = opener.open("http://ebookbrowsee.net/").read()
-soup = BeautifulSoup(html)
+while True:
+    try:
+        html = opener.open("http://ebookbrowsee.net/").read()
+        soup = BeautifulSoup(html)
+        div = soup.find('div', attrs={'class': 'rel_search'})
+        for d in div.findAll('a'):
+            if db.term.find_one({'term': d.getText()}) is None:
+                db.term.insert({'term': d.getText(), 'status': 0})
+    except:
+        print 'something error happened!, wait for 30 second'
+        time.sleep(30)
+        continue
 
-div = soup.find('div', attrs={'class': 'rel_search'})
-for d in div.findAll('a'):
-    print d.getText()
+    time.sleep(5)
+
+
 
 # format dbase
 # {'term': 'theterm', 'status': 0} # not inserted yet
