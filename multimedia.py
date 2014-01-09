9fc333030d798c14f2942f72985605dd82843b24
sopier <sopier@gmail.com>
2014-01-09 22:00:57 +0700

    adding new crawler type

---
+++ multimedia.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import gevent
+from gevent import monkey
+monkey.patch_all()
+import urllib2
+from bs4 import BeautifulSoup
+import re
+import pymongo
+from urllib import unquote
+from datetime import datetime
+
+# database thing
+c = pymongo.Connection()
+mediadb = c['files']
+
+terms = {
+    'keyword': 'crack xbox mediafire.com',
+    'type': 'rar',
+    'source': 'pastebin',
+    'source_url': 'pastebin.com',
+    'crawl': 0,
+    }
+
+google_urls = ["https://www.google.com/search?q=" + terms['keyword'].replace(' ', '+') + "+" + terms['type'] + "+\"" + terms['source_url'] + "\"" + "&num=10"]
+
+def grab(url):
+    print 'Starting %s' % url
+    # http request
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    html = opener.open(url).read()
+    # soup coming in
+    soup = BeautifulSoup(html)
+    # parsing
+    ## get title
+    titles = [i.get_text() for i in soup.findAll('h3', attrs={'class': 'r'})]
+    ## get url
+    ahrefs = [i.find('a')['href'] for i in soup.findAll('h3', attrs={'class': 'r'})]
+    pattern = re.compile(r"=(.*?)&")
+    urls = [re.search(pattern, i).group(1) for i in ahrefs]
+    ## prevent from string quoting on url
+    urls = [unquote(url) for url in urls]
+    ## get snippet
+    snippets = [i.get_text() for i in soup.findAll('span', attrs={'class': 'st'})]
+    ## gathering data
+    container = []
+    ## format data
+    if len(titles) == len(urls) == len(snippets):
+        for i in range(len(titles)):
+            container.append({'title': titles[i], 'url': urls[i], 'snippet': snippets[i]})
+    return container
+
+# join all jobs
+jobs = [gevent.spawn(grab, url) for url in google_urls]
+print 'starting to crawl...'
+gevent.joinall(jobs)
+results = [job.value for job in jobs]
+
+for i in range(len(results)): # jumlah (len) sesuai jumlah (len) keywords ex: 10
+    for r in results[i]: # jumlah (len) sesuai parameter num= ex: 100
+        r.update({'keyword': terms['keyword']})
+        r.update({'type': terms['type']})
+        r.update({'source': terms['source']})
+        r.update({'source_url': terms['source_url']})
+        r.update({'crawl': 0})
+        r.update({'added': datetime.now()})
+        mediadb.nonpdf.insert(r)
